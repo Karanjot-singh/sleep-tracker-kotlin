e11516eb5a3528fef787652836a0276a0961c477
karanjot-singh <karanjot19050@iiitd.ac.in>
2021-05-25 02:06:45 +0530

    Added Chip Navigation Bar to app

--- app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.android.trackmysleepquality
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+//        val navController = this.findNavController(R.id.)
         setContentView(R.layout.activity_main)
     }
 }
